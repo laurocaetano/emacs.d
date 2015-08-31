67fe2eaf453566573fc5ea4e364b5becf8d3f60b
Lauro Caetano <lauro.caetano@plataformatec.com.br>
2015-08-31 19:18:20 -0300

    Configure company mode

--- lisp/init-elixir.el
+++ lisp/init-elixir.el
@@ -1,6 +1,16 @@
 (require-package 'elixir-mode)
+(require-package 'company)
 (require-package 'alchemist)
 
 (setq alchemist-key-command-prefix (kbd "C-c ,"))
+
+(add-hook 'elixir-mode-hook 'alchemist-mode)
+(add-hook 'alchemist-iex-mode-hook '(lambda()
+                                      (local-set-key (kbd "<tab>") 'company-complete)))
+
+(setq company-idle-delay 0.1)
+(setq company-tooltip-limit 10)
+(setq company-minimum-prefix-length 2)
+(setq company-tooltip-flip-when-above t)
 
 (provide 'init-elixir)
